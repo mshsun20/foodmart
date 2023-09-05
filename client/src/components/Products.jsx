d4504814bbcc071a9518fb7aa10635a76e926799
mshsun20 <msh.sun20@gmail.com>
2023-09-05 18:20:46 +0530

    Update on Product page

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,20 +1,28 @@
+import { useState } from 'react'
 import Cardsdata from '../datasets/CardData'
 
 const Products = () => {
+    const [itmCnt, setItmCnt] = useState(0)
+
   return (
     <>
         <div className="pdts">
-            {
-                (Cardsdata.length>0) ? Cardsdata.map((itm, indx) => {
-                    return (
-                        <div key={indx} className="pdt">
-                            <img src={itm.imgdata} alt="" />
-                            <h3>{itm.dish}</h3>
-                            <p>{itm.address}</p>
-                        </div>
-                    )
-                }) : null
-            }
+            <div className="adcrt">{itmCnt} Items</div>
+            <div className="pdtcrd">
+                {
+                    (Cardsdata.length>0) ? Cardsdata.map((itm, indx) => {
+                        return (
+                            <div key={indx} className="pdt">
+                                <img src={itm.imgdata} alt="" />
+                                <h3>{itm.dish}</h3>
+                                <p>{itm.address}</p>
+                                <p>{itm.price}</p>
+                                <button onClick={() => setItmCnt(itmCnt+1)}>Add to Cart</button>
+                            </div>
+                        )
+                    }) : null
+                }
+            </div>
         </div>
     </>
   )
